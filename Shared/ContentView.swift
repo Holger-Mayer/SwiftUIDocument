01caa126570ef79dc581249ca621e103641a734f
Holger Mayer <holgermayer@icloud.com>
2020-09-02 09:31:36 +0200

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Holger Mayer on 02.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Binding var document: SwiftUIDocumentDocument
+
+    var body: some View {
+        TextEditor(text: $document.text)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(document: .constant(SwiftUIDocumentDocument()))
+    }
+}
---
+++ Shared/SwiftUIDocumentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDocumentApp.swift
+//  Shared
+//
+//  Created by Holger Mayer on 02.09.20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDocumentApp: App {
+    var body: some Scene {
+        DocumentGroup(newDocument: SwiftUIDocumentDocument()) { file in
+            ContentView(document: file.$document)
+        }
+    }
+}
